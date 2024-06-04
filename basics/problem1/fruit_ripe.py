a17551d2259c2251a54d9208fed6ade6ffdd2f75
AMOGHPROMODPHATARPEKAR <amoghpp129@gmail.com>
2024-06-04 23:19:10 +0530

    done conditionals

--- basics/problem1/fruit_ripe.py
+++ basics/problem1/fruit_ripe.py
@@ -1,3 +1,14 @@
-color = 'yellow'
+color = 'brown'
+fruit = "Banana"
+
+if fruit == 'Banana':
+    if color == 'green':
+        print("Unripe")
+    elif color == 'yellow':
+        print("riped")
+    elif color == 'brown':
+        print("Overriped")
+else:
+    print("No info of other fruit")
 
 
